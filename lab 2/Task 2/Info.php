a04f561ea60c965928580ee82c5c240e55043154
Mahir Mostafa <sheikhnihal02@gmail.com>
2023-02-16 20:20:05 +0600

    updated

---
+++ lab 2/Task 2/Info.php
@@ -0,0 +1,8 @@
+<?php
+ $fname =  $_REQUEST['fname'];
+ $email =  $_REQUEST['email'];
+ echo"<center>" ;
+ echo "<h3>Name:". $fname . "<h3>" ;
+ echo "<h3>Email :" . $email . "<h3>" ;
+echo "</center>";
+?>
