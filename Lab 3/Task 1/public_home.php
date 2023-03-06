3dde241cdac6236f3168b498d10f211259000769
Mahir Mostafa <sheikhnihal02@gmail.com>
2023-03-07 01:35:30 +0600

    Done

--- Lab 3/Task 1/public_home.php
+++ Lab 3/Task 1/public_home.php
@@ -13,11 +13,14 @@
         
 <table border="1" width="600px" height="">
 <tr> 
-    <td><h2><b> XCompany </b></h2></td>
+    <td><h1><b> XCompany </b> </h1>
+    <nav align="right">
+        <a href="public_home.php">Home</a>|
+        <a href="login.php">Login</a> |
+        <a href="registration.php">Registration</a>
+      </nav> 
+   </td>
 
-     <th><a href="public_home.php">Home</a></th>
-    <th><a href="login.php">Login</a></th>
-    <th><a href="registration.php">Registration</a></th>   
    
 </tr>
 <tr>
@@ -28,7 +31,7 @@
    
 </tr>
 <tr>
-    <td colspan="4"><p><center>Copyright (c) 2017</center></p></td>
+    <td><p><center>Copyright (c) 2017</center></p></td>
 </tr>
 </table>
             
